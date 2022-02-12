972cbc3c6c817e0f61b87c8807ec9c04c9fdb6a2
magmat88 <magdalena.matusiak88@gmail.com>
2022-02-12 11:00:09 +0100

    Added data

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+// import { configureStore } from '@reduxjs/toolkit';
+// import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+// export const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//   },
+// });
+
+export const store = {}
+
+const initialState = {};
+
+initialState.allMovies = [];
+initialState.favoriteMovies = [];
+initialState.searchTerm = '';
